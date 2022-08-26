861df808587882cba9927311ab6b5b7182853568
Warlen Alex <warlenalex04@gmail.com>
2022-08-26 15:04:58 -0300

    Update KC_House_price.py

--- Previsao_precos_casas_KC/KC_House_price.py
+++ Previsao_precos_casas_KC/KC_House_price.py
@@ -40,15 +40,6 @@ def agrupamento(data, col):
   aux = data[['price', col]].groupby(col).median().reset_index()  
   return(aux)
 
-def contagem(data, col):
-  aux = data[['id', col]].groupby(col).count().reset_index()
-  aux = aux.rename(columns={'id': 'Acima'}) 
-  # aux['percent'] = (aux['id'] / 
-  #                 aux['id'].sum()) * 100 
-  # print(aux)                
-  return(aux)
-
-
 def correlacao(data,variavel):
   ziptest = data[data.zipcode == variavel]
   correlation = ziptest.corr()
@@ -85,6 +76,12 @@ df['date'] = pd.to_datetime(df['date'])
 df['zipcode'] = df['zipcode'].astype(str)
 df = df.drop(['sqft_living15', 'sqft_lot15'], axis = 1 )
 
+for i in range(len(df)):
+  if df.loc[i,'bedrooms'] < 1 or df.loc[i,'bathrooms'] < 1:
+    df = df.drop(i)
+df = df.drop(15870).reset_index()	  	  
+df = df.drop(['index'], axis = 1)
+
 df['Basement?'] = yesno(df['sqft_basement'])
 df['water_view'] = yesno(df['waterfront'])
 df['Renoveted?'] = yesno(df['yr_renovated'])
@@ -99,12 +96,6 @@ df['sqft_level'] = df['sqft_living'].apply(lambda x: 1 if x <= 1430 else
                                                      3 if (x > 1920) and (x <= 2550) else 
                                                      4 if (x > 2550) and (x <= 4230) else 5)
 
-for i in range(len(df)):
-  if df.loc[i,'bedrooms'] < 1 or df.loc[i,'bathrooms'] < 1:
-    df = df.drop(i)
-df = df.drop(15870).reset_index()	  	  
-df = df.drop(['index'], axis = 1)
-
 df_zipcode = agrupamento(df,'zipcode')
 
 region = df_zipcode['zipcode'].unique()
@@ -117,8 +108,6 @@ for i in region:
 df_zipcode = df_zipcode.sort_values(by = ['price'])
 df_zipcode['price'].mean()
 df_zipcode1 = df_zipcode
-zipcode_98039 = df[df.zipcode == '98039']
-zipcode_98039.describe()
 for i in range(len(df_zipcode1)):
   if df_zipcode1.loc[i,'price'] < 501607:
     df_zipcode1 = df_zipcode1.drop(i)
@@ -130,47 +119,14 @@ for j in df_zipcode1:
     if df.loc[i,'zipcode'] == j:
       df.loc[i,'zipcode_class'] = 'acima'
     
-df['zipcode_class'].unique()
-
 df_bath = agrupamento(df,'bathrooms')
 df_grade = agrupamento(df,'grade')
 df_view = agrupamento(df,'view')
 df_bedrooms = agrupamento(df,'bedrooms')
 df_condition = agrupamento(df,'condition')
+df_waterfront = agrupamento(df,'water_view')
 
 df_high_price = df[df.Quartis_price > 3]
-
-df.columns
-
-df_countbath = contagem(df_high_price,'bathrooms')
-df_countbed = contagem(df_high_price,'bedrooms')
-df_countgrade = contagem(df_high_price, 'grade')
-df_countview = contagem(df_high_price, 'view')
-df_countwater = contagem(df_high_price, 'water_view')
-df_countcond = contagem(df_high_price, 'condition')
-
-df['level_value'].unique()
-df_acima_zipcode = df[df.level_value == 'Alto']
-df_baixo_zipcode = df[df.level_value == 'Baixo']
-
-
-df_countbath1 = contagem(df_acima_zipcode,'bathrooms')
-df_countbed1 = contagem(df_acima_zipcode,'bedrooms')
-df_countgrade1 = contagem(df_acima_zipcode, 'grade')
-df_countview1 = contagem(df_acima_zipcode, 'view')
-df_countwater1 = contagem(df_acima_zipcode, 'water_view')
-df_countcond1 = contagem(df_acima_zipcode, 'condition')
-
-df_countbath2 = contagem(df_baixo_zipcode,'bathrooms')
-df_countbed2 = contagem(df_baixo_zipcode,'bedrooms')
-df_countgrade2 = contagem(df_baixo_zipcode, 'grade')
-df_countview2 = contagem(df_baixo_zipcode, 'view')
-df_countwater2 = contagem(df_baixo_zipcode, 'water_view')
-df_countcond2 = contagem(df_baixo_zipcode, 'condition')
-
-# df_high_price.count() #5369
-# df_acima_zipcode.count() #10674
-
 
 df['mes_ano'] = df['date'].dt.strftime('%Y-%m')
 by_date = df[['price', 'mes_ano']].groupby('mes_ano').median().reset_index()
@@ -198,101 +154,6 @@ boxplot('view')
 boxplot('floors')
 boxplot('bedrooms') 
 
-fig = px.bar(df_bath, x="bathrooms", 
-             y='price', 
-             template = 'plotly_dark',
-             color_discrete_sequence=['#e1cc55', '#00224e'], 
-             labels={
-                     'price': 'Preço das casas',
-                     'bathrooms': 'Banheiros'}, 
-             title ='Preço Mediano das casas de acordo com o número de Banheiros')
-layout(fig)
-    
-hist(df,'grade')
-hist(df,'bathrooms')  
-hist(df,'bedrooms') 
-hist(df,'condition') 
-hist(df,'waterfront')
-hist(df,'view') 
-hist(df,'Renoveted?')
-hist(df,'Basement?')
-hist(df,'sazonal')
-
-fig = px.bar(sazonal, x="sazonal", 
-             y='price', 
-             template = 'plotly_dark',
-             color_discrete_sequence=['#e1cc55', '#00224e'], 
-             labels={
-                     'price': 'Preço das casas',
-                     'bathrooms': 'Banheiros'}, 
-             title ='Preço Mediano das casas de acordo com a sazonalidade', text = 'price')
-layout(fig) 
-
-fig = px.line(by_date_Baixo, x="mes_ano", y='price', title='Variação de preço das casas abaixo da média em suas regiões, durante o periodo de Maio de 2014 até maio de 2015')
-fig.show()
-
-fig = px.line(by_date_Alto, x="mes_ano", y='price', title='Variação de preço das casas acima da média em suas regiões, durante o periodo de Maio de 2014 até maio de 2015')
-fig.show()
-
-fig = px.line(by_yrbuilt_median, x="yr_built", y='price', title='Variação de preço de todas as casas, de acordo com seu ano de contrução')
-fig.show()
-fig = px.line(by_yrbuilt_Alto, x="yr_built", y='price', title='Variação de preço de todas as casas acima da mediana de suas regiões, de acordo com seu ano de contrução')
-fig.show()
-fig = px.line(by_yrbuilt_Baixo, x="yr_built", y='price', title='Variação de preço de todas as casas abaixo da mediana de suas regiões, de acordo com seu ano de contrução')
-fig.show()
-
-fig = px.bar(df_grade, x="grade", 
-             y='price', 
-             template = 'plotly_dark',
-             color_discrete_sequence=['#e1cc55', '#00224e'], 
-             labels={
-                     'price': 'Preço das casas',
-                      'grade': 'avaliação'}, 
-             title ='Preço Mediano das casas de acordo com o nivel da avaliação'
- )
-layout(fig)
-
-fig = px.bar(df_view, x="view", 
-             y='price', 
-             template = 'plotly_dark',
-             color_discrete_sequence=['#e1cc55', '#00224e'], 
-             labels={
-                     'price': 'Preço das casas',
-                      'view': 'Vista da casa'}, 
-             title ='Preço Mediano das casas de acordo com o nivel da vista da casa'
- )
-layout(fig)
-
-fig = px.bar(df_bedrooms, x="bedrooms", 
-             y='price', 
-             template = 'plotly_dark',
-             color_discrete_sequence=['#e1cc55', '#00224e'], 
-             labels={
-                     'price': 'Preço das casas',
-                      'bedrooms': 'Número de quartos da casa'}, 
-             title ='Preço Mediano das casas de acordo com o Número de quartos da casa'
- )
-layout(fig)
-
-fig = px.bar(df_condition, x="condition", 
-             y='price', 
-             template = 'plotly_dark',
-             color_discrete_sequence=['#e1cc55', '#00224e'], 
-             labels={
-                     'price': 'Preço das casas',
-                     'condition': 'Nível da condição'}, 
-             title ='Preço Mediano das casas de acordo com o nível da condição da casa'
- )
-layout(fig)
-
-fig = px.scatter(df, x="sqft_living", y='price', trendline = 'ols', trendline_color_override = '#892721', template = 'plotly_dark',labels={
-    'price': 'Preço das casas',
-    'sqft_living': 'Tamanho interno por metro quadrado'},
-    color_discrete_sequence=['#e1cc55', '#00224e'],
-    title ='Preço Mediano das casas de acordo com a área interna das casas'
- )
-layout(fig) 
-
 fig = px.box(df, x = 'price', 
               template = 'plotly_dark', 
               labels = {'price' : 'Preço'}, 
@@ -307,8 +168,77 @@ fig = px.box(df, x = 'sqft_living',
               )
 layout(fig)
 
-df_waterfront = agrupamento(df,'water_view')
+fig = px.scatter(df, x="sqft_living", y='price', trendline = 'ols', trendline_color_override = '#892721', template = 'plotly_dark',labels={
+    'price': 'Preço das casas',
+    'sqft_living': 'Tamanho interno por pés²'},
+    color_discrete_sequence=['#e1cc55', '#00224e'],
+    title ='Preço mediano das casas de acordo com a área interna'
+ )
+layout(fig) 
 
+fig = px.bar(df_bath, x="bathrooms", 
+             y='price', 
+             template = 'plotly_dark',
+             color_discrete_sequence=['#e1cc55', '#00224e'], 
+             labels={
+                     'price': 'Preço das casas',
+                     'bathrooms': 'Banheiros'}, 
+             title ='Preço mediano das casas de acordo com o número de banheiros')
+layout(fig)
+
+fig = px.bar(sazonal, x="sazonal", 
+             y='price', 
+             template = 'plotly_dark',
+             color_discrete_sequence=['#e1cc55', '#00224e'], 
+             labels={
+                     'price': 'Preço das casas',
+                     'bathrooms': 'Banheiros'}, 
+             title ='Preço Mediano das casas de acordo com a sazonalidade', text = 'price')
+layout(fig) 
+
+fig = px.bar(df_grade, x="grade", 
+             y='price', 
+             template = 'plotly_dark',
+             color_discrete_sequence=['#e1cc55', '#00224e'], 
+             labels={
+                     'price': 'Preço das casas',
+                      'grade': 'avaliação'}, 
+             title ='Preço mediano das casas de acordo com o nivel da avaliação'
+ )
+layout(fig)
+
+fig = px.bar(df_view, x="view", 
+             y='price', 
+             template = 'plotly_dark',
+             color_discrete_sequence=['#e1cc55', '#00224e'], 
+             labels={
+                     'price': 'Preço das casas',
+                      'view': 'Vista da casa'}, 
+             title ='Preço mediano das casas de acordo com o nivel da vista'
+ )
+layout(fig)
+
+fig = px.bar(df_bedrooms, x="bedrooms", 
+             y='price', 
+             template = 'plotly_dark',
+             color_discrete_sequence=['#e1cc55', '#00224e'], 
+             labels={
+                     'price': 'Preço das casas',
+                      'bedrooms': 'Número de quartos da casa'}, 
+             title ='Preço mediano das casas de acordo com o número de quartos'
+ )
+layout(fig)
+
+fig = px.bar(df_condition, x="condition", 
+             y='price', 
+             template = 'plotly_dark',
+             color_discrete_sequence=['#e1cc55', '#00224e'], 
+             labels={
+                     'price': 'Preço das casas',
+                     'condition': 'Nível da condição'}, 
+             title ='Preço mediano das casas de acordo com a condição'
+ )
+layout(fig)
 
 fig = px.bar(df_waterfront, x="water_view", 
              y='price', 
@@ -317,10 +247,9 @@ fig = px.bar(df_waterfront, x="water_view",
              labels={
                      'price': 'Preço das casas',
                      'water_view': 'Possui vista para o mar'}, 
-             title ='Preço Mediano considerando se a casa tem vista para o mar'
+             title ='Preço mediano considerando se a casa tem vista para o mar'
  )
 layout(fig)
-
 
 mapa_oficial = px.scatter_mapbox(df, lat='lat',lon='long', hover_name = 'price', 
                                  color = 'Quartis_price', 
@@ -333,14 +262,22 @@ mapa_oficial.update_layout(mapbox_style = 'carto-darkmatter')
 mapa_oficial.update_layout(height = 700, width = 750, margin = {'r':0, 't':45, 'l':0, 'b':0})
 mapa_oficial.show() 
 
+hist(df,'grade')
+hist(df,'bathrooms')  
+hist(df,'bedrooms') 
+hist(df,'condition') 
+hist(df,'waterfront')
+hist(df,'view') 
+hist(df,'Renoveted?')
+hist(df,'Basement?')
+hist(df,'sazonal')
+
 fig = px.histogram(df, x='grade', color="level_value", 
                    template = 'plotly_dark', 
                    barmode = 'group', 
                    color_discrete_sequence=['#e1cc55', '#123570'], 
                    labels = {'grade' : 'Avaliação', 'level_value': 'valor da casa'}, title = 'Quantidade de casas acima e abaixo da mediana de acordo com o nivel da avaliação ')
 layout(fig)
-
-
 
 fig = px.histogram(df, x='bathrooms', color="level_value", 
                    template = 'plotly_dark', 
@@ -373,6 +310,19 @@ fig = px.histogram(df, x='view', color="level_value",
                    labels = {'view' : 'Nível da Vista', 'level_value': 'Valor da Casa'}, title = 'Quantidade de casas acima e abaixo da mediana de acordo com o nível da vista')
 layout(fig)
 
+fig = px.line(by_date_Baixo, x="mes_ano", y='price', title='Variação de preço das casas abaixo da média em suas regiões, durante o periodo de Maio de 2014 até maio de 2015')
+fig.show()
+
+fig = px.line(by_date_Alto, x="mes_ano", y='price', title='Variação de preço das casas acima da média em suas regiões, durante o periodo de Maio de 2014 até maio de 2015')
+fig.show()
+
+fig = px.line(by_yrbuilt_median, x="yr_built", y='price', title='Variação de preço de todas as casas, de acordo com seu ano de contrução')
+fig.show()
+fig = px.line(by_yrbuilt_Alto, x="yr_built", y='price', title='Variação de preço de todas as casas acima da mediana de suas regiões, de acordo com seu ano de contrução')
+fig.show()
+fig = px.line(by_yrbuilt_Baixo, x="yr_built", y='price', title='Variação de preço de todas as casas abaixo da mediana de suas regiões, de acordo com seu ano de contrução')
+fig.show()
+
 fig = px.line(by_date, x="mes_ano", 
               y='price', width=850,
               color_discrete_sequence=['#e1cc55', '#123570'], 
@@ -381,8 +331,7 @@ fig = px.line(by_date, x="mes_ano",
               title='Variação de preço médio das casas, durante o periodo de Maio de 2014 até maio de 2015')
 layout(fig)
 #--------------------------------------------------------------------------------------
-df = pd.read_csv('kc_definitivo.csv') 
-
+# df = pd.read_csv('kc_definitivo.csv') 
 
 df = df.drop(['lat'], axis = 1)
 df = df.drop(['long'], axis = 1)
@@ -396,7 +345,6 @@ df= df.drop(['sqft_level'], axis = 1)
 df= df.drop(['zipcode_class'], axis = 1)
 
 df['zipcode'] = df['zipcode'].astype(str)
-
 
 label_encoder_zipcode = LabelEncoder()
 label_encoder_Renovated = LabelEncoder()
@@ -493,6 +441,8 @@ primeiros_resultados = pd.DataFrame(primeiros_resultados)
 
 x_kc_house = np.concatenate((x_kc_treinamento, x_kc_teste), axis = 0)
 y_kc_house = np.concatenate((y_kc_treinamento, y_kc_teste), axis = 0)
+x_kc_scaled = np.concatenate((x_kc_treinamento_scaled, x_kc_teste_scaled), axis = 0)
+y_kc_scaled = np.concatenate((y_kc_treinamento_scaled, y_kc_teste_scaled), axis = 0)
 
 parametros_random = {'n_estimators': [10,50,100,150],
              'min_samples_split' : [2,5,10],
@@ -506,9 +456,6 @@ melhores_parametros = grid_search.best_params_
 melhor_score = grid_search.best_score_
 print(melhores_parametros)
 melhor_score
-
-x_kc_scaled = np.concatenate((x_kc_treinamento_scaled, x_kc_teste_scaled), axis = 0)
-y_kc_scaled = np.concatenate((y_kc_treinamento_scaled, y_kc_teste_scaled), axis = 0)
 
 parametros_svr = {'kernel' : ['linear', 'poly', 'rbf', 'sigmoid']}
 y_kc_scaled = y_kc_scaled.ravel()
@@ -540,6 +487,7 @@ resultados_rede_neural = []
 
 for i in range(30):
     kfold = KFold(n_splits = 10, shuffle = True, random_state = i)
+
     random_forest = RandomForestRegressor(min_samples_leaf = 1,
                                           min_samples_split= 5,
                                           n_estimators= 100,
@@ -581,27 +529,6 @@ compara_algoritmos = MultiComparison(resultados_df['accuracy'], resultados_df['a
 teste_estatistico = compara_algoritmos.tukeyhsd()
 print(teste_estatistico)
 
-
-regressor_svr_kc = SVR(kernel='poly')
-regressor_svr_kc.fit(x_kc_treinamento_scaled, y_kc_treinamento_scaled.ravel())
-regressor_svr_kc.score(x_kc_treinamento_scaled, y_kc_treinamento_scaled)
-regressor_svr_kc.score(x_kc_teste_scaled, y_kc_teste_scaled)
-previsoes = regressor_svr_kc.predict(x_kc_teste_scaled)
-previsoes = previsoes.reshape(-1, 1)
-y_kc_teste_inverse = scaler_y_kc.inverse_transform(y_kc_teste_scaled)
-previsoes_inverse = scaler_y_kc.inverse_transform(previsoes)
-mean_absolute_error(y_kc_teste_inverse, previsoes_inverse)
-
-regressor_rna_kc = MLPRegressor(activation= 'relu',batch_size= 56, solver = 'sgd',max_iter=1000, hidden_layer_sizes=(9,9))
-regressor_rna_kc.fit(x_kc_treinamento_scaled, y_kc_treinamento_scaled.ravel())
-regressor_rna_kc.score(x_kc_treinamento_scaled, y_kc_treinamento_scaled)
-regressor_rna_kc.score(x_kc_teste_scaled, y_kc_teste_scaled)
-previsoes = regressor_rna_kc.predict(x_kc_teste_scaled)
-previsoes = previsoes.reshape(-1,1)
-y_kc_teste_inverse_rna = scaler_y_kc.inverse_transform(y_kc_teste_scaled)
-previsoes_inverse_rna = scaler_y_kc.inverse_transform(previsoes)
-mean_absolute_error(y_kc_teste_inverse_rna, previsoes_inverse_rna)
-
 regressor_rna_kc = MLPRegressor(activation= 'relu',batch_size= 56, solver = 'sgd',max_iter=1000, hidden_layer_sizes=(9,9))
 regressor_rna_kc.fit(x_kc_scaled, y_kc_scaled.ravel())
 regressor_rna_kc.score(x_kc_scaled, y_kc_scaled)
@@ -615,9 +542,7 @@ mean_absolute_error(y_kc_teste_inverse_rna, previsoes_inverse_rna)
 pickle.dump(regressor_rna_kc, open ('rede_neural_finalizado.sav', 'wb'))
 
 rede_neural = pickle.load(open('rede_neural_finalizado.sav','rb'))
-
-
-
+# --------------------------------------------------------------------------------------
 df.loc[1,'price']
 
 scaler_x_kc = StandardScaler()
@@ -693,9 +618,7 @@ df1_y_kc = df.iloc[:, 2].values
 # df1_x_kc.loc[0,'condition'] = 4
 # df1_x_kc.loc[0,'grade'] = 8
 # df1_x_kc.loc[0,'zipcode'] = '98115'
-df1_x_kc.loc[0,'bedrooms'] = 5
-
-
+# df1_x_kc.loc[0,'bedrooms'] = 5
 
 df1_y_kc[0]
 df1_x_kc = df1_x_kc.values
